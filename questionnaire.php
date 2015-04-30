6e71fd23ee6681cbc3e4d7fdab59ab51297962e8
Jesse Robinson <j3robinson1@gmail.com>
2015-04-30 18:21:15 -0400

    beginning of questionnaire

---
+++ questionnaire.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+  <title>Questionnaire</title>
+</head>
+<body>
+  <?php 
+
+    $answers = ['1'=>'a', '2'=>'b', '3'=>'b', '4'=>'d', '5'=>'a'];
+    $questions = ['1'=>'What is my favorite color?', '2'=>'What is OOP?', '3'=>'What does PHP stand for?', '4'=>'What is my name?', '5'=>'What is my mothers name?'];
+    foreach($questions as $question) {
+
+      $i += 1;
+      echo $i . '. ';
+      echo $question;
+      ?><br>
+      <?php
+    echo $_POST['value'];
+?>
+<form method="post" action="">
+<select>
+  <option value="a">A</option>
+  <option value="b">B</option>
+  <option value="c">C</option>
+  <option value="d">D</option>
+</select>
+<br>
+<?php
+      if($answer == $answers[$i]) {
+
+        echo "you were correct";
+
+      }
+
+    }
+    
+
+   ?>
+
+<input type="submit">
+</form>
+</body>
+</html>
